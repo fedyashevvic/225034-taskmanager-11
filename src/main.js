d08615b2cd359f17a70d23be55ba914c78291ef8
fedyashevvic <fedyashevvic@gmail.com>
2020-04-11 22:57:25 +0300

    Выполняет задание Учебный проект: шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import {returnFiltersTemplate} from "./components/filters-template.js";
 import {returnTaskTemplate} from "./components/task-template.js";
 import {returnCreateTaskTemplate} from "./components/create-task-template.js";
 import {returnLoadMoreButtonTemplate} from "./components/load-more-template.js";
+import {temprData} from "./components/temprData.js";
 
-const NUMBER_OF_RENDERED_TASKS = 3;
+const NUMBER_ON_START_TASKS = 9;
+const NUMBER_OF_NEXT_TASKS = 8;
+let shownTasks = NUMBER_ON_START_TASKS;
+
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -21,8 +25,22 @@ render(siteMainElement, returnSortTemplate());
 const siteBoardElement = siteMainElement.querySelector(`.board`);
 const siteBoardTasksElement = siteBoardElement.querySelector(`.board__tasks`);
 
-render(siteBoardTasksElement, returnCreateTaskTemplate());
-for (let i = 0; i < NUMBER_OF_RENDERED_TASKS; i++) {
-  render(siteBoardTasksElement, returnTaskTemplate());
-}
+render(siteBoardTasksElement, returnCreateTaskTemplate(temprData[0]));
+
+temprData.slice(1, NUMBER_ON_START_TASKS)
+  .forEach((task) => render(siteBoardTasksElement, returnTaskTemplate(task)));
+
 render(siteBoardElement, returnLoadMoreButtonTemplate());
+const loadMoreButton = siteBoardElement.querySelector(`.load-more`);
+
+loadMoreButton.addEventListener(`click`, () => {
+  let lastShownTasks = shownTasks;
+  shownTasks = shownTasks + NUMBER_OF_NEXT_TASKS;
+
+  temprData.slice(lastShownTasks, shownTasks)
+    .forEach((task) => render(siteBoardTasksElement, returnTaskTemplate(task)));
+
+  if (shownTasks >= temprData.length) {
+    loadMoreButton.remove();
+  }
+});
